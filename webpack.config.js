ab25b39f6fb4a650c7162e9f18f64cda54dc7f4b
mauricoro <119470826+mauricoro@users.noreply.github.com>
2023-11-24 11:24:21 -0600

    Create todo-lists and add items

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
-const path = require('path');
+const path = require("path")
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
+  mode: "production",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
-};
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+}
